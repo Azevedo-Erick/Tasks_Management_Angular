fca90853c49f79a019a981c4ef5e7be32d6c0ba7
Azevedo-Erick <azeve.erick@outlook.com>
2023-11-09 23:28:16 -0300

    feat: estilização do layout administrativo e de autorização

--- src/app/shared/layouts/admin-layout/admin-layout.component.ts
+++ src/app/shared/layouts/admin-layout/admin-layout.component.ts
@@ -1,14 +1,41 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+
+
+type LayoutRoute = {
+  name: string,
+  route: string
+}
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdminLayoutComponent {
-
+  public routes: LayoutRoute[] = [
+    {
+      name: "Teams",
+      route: "/admin/team/list"
+    },
+    {
+      name: "Tasks",
+      route: "/admin/task/list"
+    },
+    {
+      name: "Tags",
+      route: "/admin/tag/list"
+    },
+    {
+      name: "Users",
+      route: "/admin/user/list"
+    },
+  ]
 }
